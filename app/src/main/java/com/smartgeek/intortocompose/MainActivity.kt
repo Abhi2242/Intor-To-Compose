94dc04d130f0d40b43dcdbbd10eaa1e6eb06e534
Abhishek_GKB <abhishek.vengurlekar@gkbhitech.com>
2026-01-29 16:48:30 +0530

    Tab button

--- app/src/main/java/com/smartgeek/intortocompose/MainActivity.kt
+++ app/src/main/java/com/smartgeek/intortocompose/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.smartgeek.intortocompose
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.smartgeek.intortocompose.ui.theme.IntorToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +41,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IntorToComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier
+                    .fillMaxSize(),
+                    color = Color.Blue) {
+                    Column(modifier = Modifier
+                        .fillMaxSize()) {
+//                        MyApp()
+//                        CreateCircle()
+                        GreetingPreview()
+                    }
                 }
             }
         }
@@ -31,17 +57,104 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    Surface(modifier = Modifier
+        .padding(15.dp),
+        color = Color.Green) {
+        Text(text = "Hello!",
+            textAlign = TextAlign.Center,
+            color = Color.LightGray,
+            fontSize = 42.sp)
+    }
 }
+
+@Composable
+fun CreateCircle() {
+    var toastMessage by remember { mutableStateOf<String?>(null) }
+
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .size(100.dp),
+        colors = CardDefaults.cardColors(Color.Cyan),
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable {
+                    Log.d("Button Test", "Button Clicked")
+                    toastMessage = "Button Clicked"
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Tap", color = Color.Black)
+        }
+    }
+
+    // ✅ Only show toast when message is not null
+    toastMessage?.let {
+        CustomToast(
+            message = it,
+            onShown = { toastMessage = null } // ✅ reset state
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntorToComposeTheme {
-        Greeting("Android")
+        Column(modifier = Modifier
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            MyApp()
+            CreateCircle()
+        }
     }
 }
+
+/** Side-effects functions */
+
+@Composable
+fun CustomToast(
+    message: String,
+    onShown: () -> Unit
+) {
+    val context = LocalContext.current
+
+    LaunchedEffect(message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        onShown()
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier,
+//        color = Color.Black
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun ShowAge(age: Int = 12) {
+//    Text(text = age.toString(),
+//        color = Color.Black
+//    )
+//}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    IntorToComposeTheme {
+//        Column {
+//            Greeting("Abhishek")
+//            ShowAge(age = 27)
+//        }
+//    }
+//}
